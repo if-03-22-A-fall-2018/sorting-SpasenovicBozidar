84249d4acf388f256bce2eb855b8b64d77a9238a
SpasenovicBozidar <spasenovic2002@gmail.com>
2019-05-18 15:24:42 +0200

    finish bubble_sort

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -16,13 +16,22 @@
 void 	init_random (int *array, unsigned long length){
   for (int i = 0; i < (int)length; i++) {
     array[i]= rand();
-  }
+}
 }
 
 void 	bubble_sort (int *array, unsigned long length){
-
+    int swap = 0;
+    for (int i = 0; i < (int)length; i++) {
+      for (int j = 0; j < (int)length; j++) {
+          if (array[i] < array[j]) {
+            swap = array[i];
+            array[i] = array[j];
+            array[j] = swap;
+          }
+      }
+    }
 }
 
 void 	insertion_sort (int *array, unsigned long length){
-
+    
 }
