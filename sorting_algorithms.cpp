ffa2ac058d3105de53adfc17773f2f2dd57ef234
SpasenovicBozidar <spasenovic2002@gmail.com>
2019-05-18 15:19:32 +0200

    Stopwatch and init_random finished

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -10,3 +10,19 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <stdlib.h>
+#include "sorting_algorithms.h"
+
+void 	init_random (int *array, unsigned long length){
+  for (int i = 0; i < (int)length; i++) {
+    array[i]= rand();
+  }
+}
+
+void 	bubble_sort (int *array, unsigned long length){
+
+}
+
+void 	insertion_sort (int *array, unsigned long length){
+
+}
--- stopwatch.cpp
+++ stopwatch.cpp
@@ -10,3 +10,25 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include <time.h> //libary for the time
+#include "stopwatch.h"
+
+static clock_t startingPoint;
+static clock_t endingPoint;
+
+void start_stopwatch (){
+  startingPoint=clock(); //initialize the star time with the time in that moment
+  endingPoint=0; //initialize the end time with a default value
+}
+
+void stop_stopwatch (){
+  endingPoint=clock();//initialize the time to a temp variable before stopping the time
+}
+
+float elapsed_time (){
+  if(endingPoint==0)
+  {
+    stop_stopwatch();
+  }
+  return (float)(endingPoint-startingPoint)/CLOCKS_PER_SEC; //cast to float because the value of the quotient is a double
+}
